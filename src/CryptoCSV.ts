9236600cd08b54e7ea0d75191e320da850aadca8
Anthony Shuker <anthony.shuker@fleetondemand.com>
2021-02-02 11:51:16 +0000

    Fix argument output

--- src/CryptoCSV.ts
+++ src/CryptoCSV.ts
@@ -9,6 +9,7 @@ export default class CryptoCSV {
   async run(args: typeof CryptoCSVArguments): Promise<void> {
     try {
       const data = await this.scraper.get(args.exchange, args.pair, args.timeframe)
+      console.log(args.output)
       this.csvCreator.create(args.output, data)
     } catch (err) {
       console.error(err)
