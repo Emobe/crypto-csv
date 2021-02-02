6ae337d91a49d9b3170157a40d7317260c0096c5
Anthony Shuker <anthony.shuker@fleetondemand.com>
2021-02-02 11:35:38 +0000

    give csv create correct return type

--- src/CSVCreator.ts
+++ src/CSVCreator.ts
@@ -4,10 +4,14 @@ import * as ccxt from 'ccxt'
 export default class CSVCreator {
   private headers: string[] = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
 
-  create(name: string, series: ccxt.OHLCV[]): void {
-    csvwriter.createArrayCsvWriter({
-      header: this.headers,
-      path: name + '.csv'
-    }).writeRecords(series)
+  async create(name: string, series: ccxt.OHLCV[]): Promise<void> {
+    try {
+      await csvwriter.createArrayCsvWriter({
+        header: this.headers,
+        path: name + '.csv'
+      }).writeRecords(series)
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
